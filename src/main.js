4b56fc5942210b8f000e22820b053b532f82369f
ศุภรัตน์ กลิ่นสังข์ <meenanusak@gmail.com>
2020-11-04 05:12:12 +0700

    .

--- src/main.js
+++ src/main.js
@@ -1,22 +1,38 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUmbrella } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
- 
-library.add(faUmbrella)
- 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUmbrella } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-Vue.config.productionTip = false
+import VueFusionCharts from "vue-fusioncharts";
+import FusionCharts from "fusioncharts";
+import FusionMaps from "fusioncharts/maps/es/fusioncharts.world";
+import World from "fusioncharts/fusioncharts.maps";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+import Thailand from "fusionmaps/maps/fusioncharts.thailand";
+
+library.add(faUmbrella);
+
+Vue.use(
+  VueFusionCharts,
+  FusionCharts,
+  FusionMaps,
+  World,
+  FusionTheme,
+  Thailand
+);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
